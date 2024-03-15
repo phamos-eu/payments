ae100581b4224f22aaa95d0061540eb9460e027e
Robera16 <roberaworknehrobwork@gmail.com>
2024-03-15 12:58:38 +0300

    feat: Add Bank Transaction

---
+++ erpnextfints/erpnextfints/doctype/payment_wizard_setting/payment_wizard_setting.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, jHetzer and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class PaymentWizardSetting(Document):
+	pass
